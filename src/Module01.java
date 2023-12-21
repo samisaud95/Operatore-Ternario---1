55683f6a058976a4512dcd1f7dbcf5be1d414c97
Samira saud <samirasaudit@gmail.com>
2023-12-21 22:56:23 +0100

    Fatto

---
+++ src/Module01.java
@@ -0,0 +1,19 @@
+public class Module01 {
+    public static void main(String[] args) {
+        System.out.println(checkString("Questa è una stringa"));
+    }
+
+    private static String checkString(String value) {
+       // String result;
+       // if (value.length() >= 10) {
+           // result = "Lunghezza maggiore o uguale di 10";
+      //  } else {
+         //   result = "Lunghezza minore di 10";
+       // }
+       // return result;
+
+        String result = (value.length() >= 10) ? "Lunghezza maggiore o uguale di 10" : "Lunghezza minore di 10";
+        return result;
+    }
+
+}
